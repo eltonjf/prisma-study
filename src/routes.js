1757e97b5f45fd339f578d6720161067070bb7e6
Elton <eltonjf@gmail.com>
2022-02-04 00:00:34 -0300

    Crud Works

--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,14 @@
 import { Router } from "express";
-import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+import UserController from "./controller/UserController";
 
 const router = Router();
 
-router.post("/user", async (req, res) => {
-
-  try {
-    const { name, email } = req.body;
-
-    //Verifica se o e-mail é único
-    const user = await prisma.user.findUnique({ where: { email } });
-
-    if(user){
-        return res.json({ error: "Já existe um usuário com esse e-mail" })
-    }
-
-    user = await prisma.user.create({
-      data: {
-        name,
-        email,
-      },
-    });
-
-    return res.json(user);
-  } catch (error) {
-    
-  }
-});
+router.post("/user", UserController.createUser);
+router.get("/users", UserController.findAllUsers);
+router.get("/user/:id", UserController.findUser);
+router.put("/user/:id", UserController.updateUser);
+router.delete("/user/:id", UserController.deleteUser);
 
 export { router };
+ 
